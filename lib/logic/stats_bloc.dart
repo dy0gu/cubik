665b9a4b761fc4743f7704e12fd4954409c738cf
Dy0gu <dbsimoes20@gmail.com>
2024-02-13 16:40:59 +0000

    Add setup for game amount tracking

--- lib/logic/stats_bloc.dart
+++ lib/logic/stats_bloc.dart
@@ -3,8 +3,10 @@ import "package:equatable/equatable.dart";
 
 class Statistics extends Equatable {
   final List<int> previousGameMoves;
+  final Map<DateTime, int> gamesPlayedPerDay;
 
-  const Statistics({required this.previousGameMoves});
+  const Statistics(
+      {this.previousGameMoves = const [], this.gamesPlayedPerDay = const {}});
 
   @override
   List<Object> get props => [previousGameMoves];
@@ -19,28 +21,38 @@ class StatisticsGameRecorded extends StatisticsEvent {
 }
 
 class StatisticsBloc extends HydratedBloc<StatisticsEvent, Statistics> {
-  StatisticsBloc() : super(const Statistics(previousGameMoves: [])) {
+  StatisticsBloc() : super(const Statistics()) {
     on<StatisticsGameRecorded>((event, emit) {
       // Limit the number of previous game moves tracked to 100
       final previousGameMoves = state.previousGameMoves.length < 100
           ? [...state.previousGameMoves, event.moves]
           : [...state.previousGameMoves.sublist(1), event.moves];
-      emit(Statistics(previousGameMoves: previousGameMoves));
+      // Limit the number of games played per day tracked to the last year
+      final gamesPlayedPerDay = state.gamesPlayedPerDay;
+      emit(Statistics(
+          previousGameMoves: previousGameMoves,
+          gamesPlayedPerDay: gamesPlayedPerDay));
     });
   }
 
   @override
   Statistics fromJson(Map<String, dynamic> json) {
-    if (json["previousGameMoves"] is List) {
+    if (json["previousGameMoves"] is List &&
+        json["previousGameMoves"].isNotEmpty) {
       return Statistics(
-          previousGameMoves: List<int>.from(json["previousGameMoves"]));
+          previousGameMoves: List<int>.from(json["previousGameMoves"]),
+          gamesPlayedPerDay:
+              Map<DateTime, int>.from(json["gamesPlayedPerDay"]));
     } else {
-      return const Statistics(previousGameMoves: []);
+      return const Statistics();
     }
   }
 
   @override
   Map<String, dynamic> toJson(Statistics state) {
-    return {"previousGameMoves": state.previousGameMoves};
+    return {
+      "previousGameMoves": state.previousGameMoves,
+      "gamesPlayedPerDay": state.gamesPlayedPerDay
+    };
   }
 }
