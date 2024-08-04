0cd83d53e80cdd81012f420ed848f55feb9e0bfa
dy0gu <dbsimoes20@gmail.com>
2024-08-04 12:11:31 +0100

    Attempt base url fallback

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import "package:flutter/material.dart";
 import "package:cubik/app.dart";
-import "package:url_strategy/url_strategy.dart";
 import "package:hydrated_bloc/hydrated_bloc.dart";
 import "package:path_provider/path_provider.dart";
 import "package:flutter/foundation.dart";
+// import "package:url_strategy/url_strategy.dart";
 
 void main() async {
   // Ensures that all widget render logic is initialized before continuing
   WidgetsFlutterBinding.ensureInitialized();
 
   // Hides the # in the web version URL
-  setPathUrlStrategy();
+  // Currently breaks routing on web refreshes outside of the root path
+  // setPathUrlStrategy();
 
   // Initializes the storage for bloc persistence
   HydratedBloc.storage = await HydratedStorage.build(
