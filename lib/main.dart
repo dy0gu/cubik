4c53c531862c288c8eeff5c5d17776da2a805f49
dy0gu <dbsimoes20@gmail.com>
2024-08-04 21:59:27 +0100

    Remove url strategy dependency

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,10 @@ import "package:cubik/app.dart";
 import "package:hydrated_bloc/hydrated_bloc.dart";
 import "package:path_provider/path_provider.dart";
 import "package:flutter/foundation.dart";
-import "package:url_strategy/url_strategy.dart";
 
 void main() async {
   // Ensures that all widget render logic is initialized before continuing
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Hides the # in the web version URL, the <base> tag in the web index.html file
-  // must be set with href="/", for this to work correctly
-  setPathUrlStrategy();
 
   // Initializes the storage for bloc persistence
   HydratedBloc.storage = await HydratedStorage.build(
